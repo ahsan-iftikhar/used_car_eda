6fb56f6303bc9a048fd7ecd6e37dffb54f193a14
Ahsan Iftikhar, Latitude 5480 <ahsan.iftikhar.qureshi@gmail.com>
2022-03-27 22:20:07 +0400

    Made basic treemap

--- treemap_visualization.R
+++ treemap_visualization.R
@@ -1,3 +1,23 @@
 # Loading packages
 library(tidyverse)
-library(treemap)
+library(treemapify)
+
+# Loading data
+olx_car_data <- read_csv("data/OLX_Car_Data_CSV.csv")
+
+glimpse(olx_car_data)
+
+brnd_n_milag <- olx_car_data %>% 
+  group_by(`Brand`) %>% 
+  count() %>% 
+  arrange(desc(n)) %>% 
+  drop_na()
+
+
+ggplot(data = brnd_n_milag, 
+       mapping = aes(area = n, label = Brand)
+       ) +
+  geom_treemap() +
+  geom_treemap_text(reflow = TRUE, grow = TRUE, colour = "white")
+
+
