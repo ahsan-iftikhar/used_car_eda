c142633f7298d6fd8835cdf2c4fa6068b0be69f3
Ahsan Iftikhar, Latitude 5480 <ahsan.iftikhar.qureshi@gmail.com>
2022-03-28 21:30:58 +0400

    1) Treemap has been made. 2) Next I am after the fact what is the rate of decline in the price of the same vehicle, year on year. 3) Third I want to ascertain, the range of prices for the vehicles sold in the same year. This should be depicted as table.
    Certain learning points that I should updae in RemNotes is the following.
    1) Incorporating google fonts in ggplot, using showtext
    2) Cedric detils to make good margins, so that the graph does not spill from the side of the canvas

--- treemap_visualization.R
+++ treemap_visualization.R
@@ -1,6 +1,14 @@
 # Loading packages
 library(tidyverse)
 library(treemapify)
+library(paletteer)
+library(ggtext)
+library(showtext)
+
+font_add_google(name = "Lora", family = "title_font")
+font_add_google(name = "Roboto", family = "body_font")
+
+showtext_auto()
 
 # Loading data
 olx_car_data <- read_csv("data/OLX_Car_Data_CSV.csv")
@@ -13,11 +21,36 @@ brnd_n_milag <- olx_car_data %>%
   arrange(desc(n)) %>% 
   drop_na()
 
+dim(brnd_n_milag)
 
-ggplot(data = brnd_n_milag, 
-       mapping = aes(area = n, label = Brand)
+ggplot(data = brnd_n_milag %>% 
+         filter(!`Brand` %in% c("Toyota", "Suzuki", "Honda", "Daihatsu",
+                                "Mitsubishi", "Nissan")), 
+       mapping = aes(area = n, label = Brand, fill = Brand)
        ) +
   geom_treemap() +
-  geom_treemap_text(reflow = TRUE, grow = TRUE, colour = "white")
+  geom_treemap_text(colour = "white", reflow = TRUE) +
+  scale_fill_paletteer_d(`"ggsci::default_igv"`) +
+  labs(
+    title = "**Car brands that feature on OLX**",
+    subtitle = "You have much greater options to choose from Suzuki",
+    caption = "OLX Data Sourced from Kaggle \n "
+  ) +
+  theme(
+    legend.position = "none",
+    plot.caption = element_text(size = 7),
+    text = element_text(family = "body_font", size = 10),
+    plot.title = element_markdown(family = "title_font", size = 16,
+                                  color = "#0f3e63"),
+    plot.margin = margin(3, 3, 3, 3, "mm")
+  ) 
 
+glimpse(olx_car_data)
+
+ggplot(olx_car_data, 
+       aes(x = Year, y = Price)) + 
+  geom_point(alpha = 0.5) +
+  geom_smooth() +
+  scale_y_log10() +
+  theme_minimal()
 
